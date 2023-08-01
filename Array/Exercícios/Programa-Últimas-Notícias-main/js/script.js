01dad18181ed47347d97638dcacc8bdcd9dcfdc7
Rafael Dias <raffa96dias@gmail.com>
2023-08-01 12:13:17 -0300

    correção

--- Array/Exercícios/Programa-Últimas-Notícias-main/js/script.js
+++ Array/Exercícios/Programa-Últimas-Notícias-main/js/script.js
@@ -5,20 +5,28 @@ let outNum = document.getElementById("outNum");
 let outAnswer = document.getElementById("outAnswer");
 
 let newsArray = [];
-
 function newsAdd () {
+    if(news.value == "") {
+        alert("inclua uma notícia para ser cadastrada");
+        return;
+    }
+
     let inNews = news.value;
     newsArray.push(inNews);
-    //console.log(newsArray)
     outNum.textContent = newsArray.length;
-
+    news.value = "";
+    news.focus();
 }
 btnAdd.addEventListener("click", newsAdd)
 
 function lastNews () {
     let newsNum = prompt("Quantas notícias deseja exibir?");
-    for (let i = news.length -1; i < newsArray.length; i --) {
-        
+    text = `${newsNum} Última(s) Notícias\n------------------------------------------------------\n`;
+
+    for (let i = newsArray.length - 1; i >= newsArray.length  - newsNum; i --) {
+        text += `${i+1}º) ${newsArray[i]} \n`;
     }
+    outAnswer.textContent = text;
+    
 }
 btnNews.addEventListener("click", lastNews);
